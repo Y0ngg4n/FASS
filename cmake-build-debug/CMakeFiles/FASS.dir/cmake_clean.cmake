14ece5f00d54a0906fedec2abe9dbbe3e6539eb5
Yonggan <yonggan@oblivioncoding.pro>
2021-11-27 11:43:43 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/FASS.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FASS_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/FASS_autogen.dir/ParseCache.txt"
+  "FASS_autogen"
+  "CMakeFiles/FASS.dir/FASS_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/FASS.dir/main.cpp.o"
+  "FASS"
+  "FASS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FASS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
