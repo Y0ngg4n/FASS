d2d85553aacbdcc36e9e6020e0132d632506a8c0
Yonggan <yonggan@oblivioncoding.pro>
2021-11-28 14:35:25 +0100

    Add basic design

--- main.cpp
+++ main.cpp
@@ -3,27 +3,56 @@
 #include "utils/config.h"
 #include "main.h"
 #include "package_managers/PackageManager.h"
+#include "utils/border_layout.h"
 #include<QDebug>
 #include <QSettings>
-#include <QGridLayout>
+#include <QLineEdit>
+
+
+const int iconSize = 50;
+const int searchBarHeight = 25;
 
 int Main::start(int argc, char *argv[]) {
+
     qDebug() << "Starting FASS";
     QSettings *settings = makeConfig();
     PMType pmType = getPMType(settings->value("package_manager").toString());
     PackageManager::sync(pmType, false);
     QApplication a(argc, argv);
-    QPushButton *search = new QPushButton(QIcon::fromTheme("search"), nullptr);
-    QPushButton *installed = new QPushButton(QIcon::fromTheme("installed"), nullptr);
-    QPushButton *update = new QPushButton(QIcon::fromTheme("update"), nullptr);
+    QHBoxLayout *topLayout = new QHBoxLayout;
+    QVBoxLayout *leftLayout = new QVBoxLayout;
+    QLineEdit *searchbar = new QLineEdit();
+    searchbar->setClearButtonEnabled(true);
+    QFont font = searchbar->font();
+    font.setPointSize(searchBarHeight);
+    searchbar->setFont(font);
+//    QPushButton *search = new QPushButton(QIcon::fromTheme("search"), nullptr);
+    searchbar->setAlignment(Qt::AlignCenter);
+    searchbar->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    QPushButton *installed = new QPushButton(QIcon::fromTheme("download"), nullptr);
+    installed->setIconSize(QSize(iconSize, iconSize));
     QPushButton *refreshButton = new QPushButton(QIcon::fromTheme("reload"), nullptr);
+    refreshButton->setIconSize(QSize(iconSize, iconSize));
     QWidget *window = new QWidget;
-    QHBoxLayout *topLayout = new QHBoxLayout(window);
-    topLayout->addWidget(search);
-    topLayout->addWidget(installed);
-    topLayout->addWidget(update);
-    topLayout->addWidget(refreshButton);
+    BorderLayout *borderLayout = new BorderLayout;
+    QVBoxLayout *leftBottomLayout = new QVBoxLayout;
+    QVBoxLayout *leftTopLayout = new QVBoxLayout;
+    borderLayout->add(leftLayout, BorderLayout::West);
+    borderLayout->add(topLayout, BorderLayout::North);
+    leftLayout->addLayout(leftTopLayout);
+    leftLayout->addLayout(leftBottomLayout);
+    leftBottomLayout->addWidget(installed);
+    leftBottomLayout->addWidget(refreshButton);
+    leftBottomLayout->setAlignment(Qt::AlignBottom);
+    topLayout->setAlignment(Qt::AlignCenter);
+    leftLayout->setContentsMargins(10, 10, 10, 10);
+    topLayout->setContentsMargins(iconSize + 30, 10, iconSize + 30, 10);
+    topLayout->addWidget(searchbar);
+//    topLayout->addWidget(search);
+
+    window->setLayout(borderLayout);
     window->show();
+    window->setMinimumSize(400, 400);
     return QApplication::exec();
 }
 
